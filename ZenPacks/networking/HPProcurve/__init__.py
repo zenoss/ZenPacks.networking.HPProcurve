79bb32b44e1fbf74a985253a4b63354c15bb21fc
Chet Luther <cluther@zenoss.com>
2012-04-02 10:52:25 -0500

    Auto-create prerequisite organizers.
    * Increment version to 1.1.1.
    * Creates /Network/Switch/HP device class.
    * Creates /Net/traps event class.

--- ZenPacks/networking/HPProcurve/__init__.py
+++ ZenPacks/networking/HPProcurve/__init__.py
@@ -1,8 +1,37 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2008, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 
 import Globals
-import os.path
 
-skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
-from Products.CMFCore.DirectoryView import registerDirectory
-if os.path.isdir(skinsDir):
-    registerDirectory(skinsDir, globals())
+from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
+from Products.ZenUtils.Utils import unused
+
+unused(Globals)
+
+
+class ZenPack(ZenPackBase):
+    def install(self, app):
+        """Custom install method for this ZenPack."""
+        self.pre_install(app)
+        super(ZenPack, self).install(app)
+
+    def pre_install(self, app):
+        """Perform steps that should be done before default install."""
+        devices = app.zport.dmd.Devices
+        events = app.zport.dmd.Events
+
+        # /Network/Switch/HP device class is a prerequisite.
+        devices.createOrganizer('/Network/Switch/HP')
+
+        # /Net/traps event class is a prerequisite.
+        events.createOrganizer('/Net/traps')
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.networking.HPProcurve'
-VERSION = '1.1'
+VERSION = '1.1.1'
 AUTHOR = 'Florian Deckert'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.networking']
@@ -33,39 +33,34 @@ setup(
     # This ZenPack metadata should usually be edited with the Zenoss
     # ZenPack edit page.  Whenever the edit page is submitted it will
     # overwrite the values below (the ones it knows about) with new values.
-    name = NAME,
-    version = VERSION,
-    author = AUTHOR,
-    license = LICENSE,
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    license=LICENSE,
 
     # This is the version spec which indicates what versions of Zenoss
     # this ZenPack is compatible with
-    compatZenossVers = COMPAT_ZENOSS_VERS,
+    compatZenossVers=COMPAT_ZENOSS_VERS,
 
     # previousZenPackName is a facility for telling Zenoss that the name
     # of this ZenPack has changed.  If no ZenPack with the current name is
     # installed then a zenpack of this name if installed will be upgraded.
-    prevZenPackName = PREV_ZENPACK_NAME,
+    prevZenPackName=PREV_ZENPACK_NAME,
 
     # Indicate to setuptools which namespace packages the zenpack
     # participates in
-    namespace_packages = NAMESPACE_PACKAGES,
+    namespace_packages=NAMESPACE_PACKAGES,
 
     # Tell setuptools what packages this zenpack provides.
-    packages = find_packages(),
+    packages=find_packages(),
 
     # Tell setuptools to figure out for itself which files to include
     # in the binary egg when it is built.
-    include_package_data = True,
+    include_package_data=True,
 
     # Tell setuptools what non-python files should also be included
     # with the binary egg.
-    package_data = {
-         '': ['*.txt'],
-         '':['../COPYRIGHT.txt','../LICENSE.txt'],
-         NAME: ['objects/*','skins/*/*','services/*', 'reports/*/*',
-                'modeler/*/*', 'daemons/*', 'lib/*', 'libexec/*'],
-         },
+    #package_data = {}
 
     # Indicate dependencies on other python modules or ZenPacks.  This line
     # is modified by zenoss when the ZenPack edit page is submitted.  Zenoss
@@ -73,14 +68,14 @@ setup(
     # list, so any manual additions should be added to the end.  Things will
     # go poorly if this line is broken into multiple lines or modified to
     # dramatically.
-    install_requires = INSTALL_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
 
     # Every ZenPack egg must define exactly one zenoss.zenpacks entry point
     # of this form.
-    entry_points = {
+    entry_points={
         'zenoss.zenpacks': '%s = %s' % (NAME, NAME),
     },
 
     # All ZenPack eggs must be installed in unzipped form.
-    zip_safe = False,
+    zip_safe=False,
 )
